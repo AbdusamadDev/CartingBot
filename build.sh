a2e544e1a82fbfedf7f49713865f7c1ac412c9c3
Nodirkhon Amanhojayev <amanxojayev@gmail.com>
2024-04-26 06:45:29 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 cd /srv/project/carting.uz-bot/
 source .venv/bin/activate
 pip install -r requirements.txt
+sudo chown -R www-data:www-data /srv/project/carting.uz-bot
 sudo systemctl restart carding.bot.service
