11ae61724bd10f4eb025281546268ed1c6b137a6
Nodirkhon Amanhojayev <amanxojayev@gmail.com>
2024-04-26 05:58:38 +0000

    Add new file

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /srv/project/carting.uz-bot/
+la 
+# if sudo npm i; then
+#     if sudo npm run build; then
+#         echo "build success"
+#     else
+#         sudo mv /srv/project/carting.uz-frontend/dist_old /srv/project/carting.uz-frontend/dist
+#         sudo chown -R www-data:www-data /srv/project/carting.uz-frontend
+#         echo "error nmp i! return old dir"
+#         exit 130
+#     fi
+# else
+#     sudo mv /srv/project/carting.uz-frontend/dist_old /srv/project/carting.uz-frontend/dist
+#     sudo chown -R www-data:www-data /srv/project/carting.uz-frontend
+#     echo "error nmp run build! return old dir"
+#     exit 130
+# fi
