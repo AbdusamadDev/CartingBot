bfda1c4c5e5bc1c071d73d82ab8621372f4677e9
Nodirkhon Amanhojayev <amanxojayev@gmail.com>
2024-04-26 06:42:54 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 cd /srv/project/carting.uz-bot/
 ls -lart
-# if sudo npm i; then
-#     if sudo npm run build; then
-#         echo "build success"
-#     else
-#         sudo mv /srv/project/carting.uz-frontend/dist_old /srv/project/carting.uz-frontend/dist
-#         sudo chown -R www-data:www-data /srv/project/carting.uz-frontend
-#         echo "error nmp i! return old dir"
-#         exit 130
-#     fi
-# else
-#     sudo mv /srv/project/carting.uz-frontend/dist_old /srv/project/carting.uz-frontend/dist
-#     sudo chown -R www-data:www-data /srv/project/carting.uz-frontend
-#     echo "error nmp run build! return old dir"
-#     exit 130
-# fi
+source .venv/bin/activate
+pip install -r requirements.txt
+sudo systemctl restart carding.bot.service
