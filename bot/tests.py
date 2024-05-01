5220f1ca2f874e1cbec9b009d82c2702e53a3708
AbdusamadDev <abdusamaddev571@gmail.com>
2024-05-01 18:09:16 +0500

    Run public

---
+++ bot/tests.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        """
+        s  a  l  a  l  w  s  s  a  s  o  m
+        0  1  2  3  4  5  6  7  8  9  10  11
+        """
+        for index, letter in enumerate(s):
+            pass
