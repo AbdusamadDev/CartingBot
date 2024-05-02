10f1df6eb8eae805a0e7dd42d4543c3b9ccb12c4
AbdusamadDev <abdusamaddev571@gmail.com>
2024-05-02 16:17:31 +0500

    Update database.py

--- bot/conf.py
+++ bot/conf.py
@@ -4,6 +4,5 @@ import os
 
 TOKEN="6998603334:AAEUXAIxN4aN9q3VfqYmAk90ZE0ER6m2i60"
 DOMAIN="https://new-api.carting.uz"
-BASE_DIR = os.path.join("C:/Users/Abdusamad/Documents/Backend/carting.uz-bot/")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
